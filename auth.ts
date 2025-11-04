44af02ba61db3a40b70852251078a29b1f1158ea
zack6yuan <zack6yuan@gmail.com>
2025-11-04 00:48:50 -0600

    updating the pages.

--- auth.ts
+++ auth.ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
@@ -6,7 +7,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     logo: "/logo.png",
     buttonText: "#ffffff",
   },
-  providers: [],
+  providers: [GitHub],
   callbacks: {
     authorized: async ({ auth }) => {
       // Logged in users are authenticated, otherwise redirect to login page
