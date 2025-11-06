2a58782ee0882c8ec974b0636b3a46653dd38294
zack6yuan <zack6yuan@gmail.com>
2025-11-06 14:42:31 -0600

    all of the pages are set up.

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,19 +1,15 @@
+// Favorites Page
 import NavBar from "../components/NavBar";
 import SideBar from "../components/SideBar";
 
 export default function Favorites() {
   return (
-    <div className="">
-      <div className="text-white">
-        <h1>Hello</h1>
-      </div>
-      <NavBar />
-      <div className="">
+    <div className="flex items-center justify-center h-screen">
+        <NavBar />
         <SideBar />
-      </div>
-      <div className="text-white w-20 h-20">
-        <h1>Favorites</h1>
-      </div>
+        <div className="flex justify-start font-bold text-5xl">
+            <h1>Favorites</h1>
+        </div>
     </div>
   );
 }
--- app/watch-later/page.tsx
+++ app/watch-later/page.tsx
@@ -1,7 +1,15 @@
-export default function WatchLater() {
-    return (
-        <div className="">
-            <h1>Watch Later Page</h1>
+// Watch Later Page
+import NavBar from "../components/NavBar";
+import SideBar from "../components/SideBar";
+
+export default function WaterLater() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+        <NavBar />
+        <SideBar />
+        <div className="flex justify-start font-bold text-5xl">
+            <h1>Watch Later</h1>
         </div>
-    )
+    </div>
+  );
 }
