f67564ef5cebba905a154e175f05e65026ea8d9c
zack6yuan <zack6yuan@gmail.com>
2025-11-13 16:09:14 -0600

    pagination is complete.

--- app/components/PaginatedMovieTiles.tsx
+++ app/components/PaginatedMovieTiles.tsx
@@ -41,15 +41,15 @@ export default function MovieTiles() {
         <img
           src={movieData?.title?.[4]?.image}
           alt="placeholder image"
-          className="rounded-2xl border-2 border-[#1dd2af]"
+          className="rounded-2xl border-2 border-[#1dd2af] xl:-mt-89 2xl:-mt-151"
         />
-        <div className="absolute opacity-0 group-hover:opacity-100 transition duration-300 sm:top-1 md:-left-75 md:-top-1 lg:-left-55 xl:-left-77 xl:top-8 2xl:top-120 2xl:-left-65">
+        <div className="absolute opacity-100 group-hover:opacity-100 transition duration-300 sm:top-1 md:-left-75 md:top-10 lg:-left-55 lg:top-1 xl:-left-77 xl:top-8 2xl:top-120 2xl:-left-65">
           <ButtonOptions />
         </div>
-        <div className="absolute opacity-0 group-hover:opacity-100 transition duration-300 2xl:top-177 xl:top-55 lg:top-68 md:top-58 sm:top-105 sm:w-170 sm:h-65 bg-[#000061] 2xl:w-103 2xl:h-45 xl:w-90 xl:h-45 lg:w-113 lg:h-45 md:w-93 md:h-35 rounded-b-2xl text-sm border-l-2 border-r-2 border-b-2 border-[#1dd2af]">
+        <div className="absolute opacity-100 group-hover:opacity-100 transition duration-300 2xl:top-177 xl:top-55 lg:top-70 md:top-58 sm:top-105 sm:w-170 sm:h-65 bg-[#000061] 2xl:w-103 2xl:h-45 xl:w-90 xl:h-45 lg:w-113 lg:h-45 md:w-93 md:h-35 rounded-b-2xl text-sm border-l-2 border-r-2 border-b-2 border-[#1dd2af]">
           <div className="2xl:mt-5 2xl:ml-2 sm:ml-5 sm:mt-2 md:ml-0.5">
             <p className="2xl:ml-3 2xl:mt-2 font-bold 2xl:text-xl xl:text-xl md:text-lg xl:mt-4 xl:ml-5 lg:text-lg lg:ml-4 lg:mt-4 md:ml-4 md:mt-2 md:mb-1 sm:text-2xl sm:mb-10 sm:mt-3">
-              {movieData?.title?.[4]?.title} ({movieData?.title?.[0]?.released})
+              {movieData?.title?.[4]?.title} ({movieData?.title?.[4]?.released})
             </p>
             <p className="2xl:ml-3 2xl:mt-1 2xl:mr-3 2xl:text-lg xl:ml-5 xl:mr-3 xl:mt-3 xl:text-md lg:text-md lg:ml-4 lg:mr-5 lg:mt-5 md:ml-4 md:mr-4 md:text-sm font-light sm:text-xl sm:mr-5 sm:mb-16">
               {movieData?.title?.[4]?.synopsis}
@@ -64,21 +64,113 @@ export default function MovieTiles() {
         <img
           src={movieData?.title?.[5]?.image}
           alt="placeholder image"
-          className="rounded-2xl border-2 border-[#1dd2af]"
+          className="rounded-2xl border-2 border-[#1dd2af] xl:-mt-89 2xl:-mt-151"
         />
-        <div className="absolute opacity-0 group-hover:opacity-100 transition duration-300 sm:top-176 md:left-33 md:-top-1 lg:left-72 xl:top-8 xl:left-33 2xl:left-63 2xl:top-120">
+        <div className="absolute opacity-100 group-hover:opacity-100 transition duration-300 sm:top-176 md:left-33 md:top-10 lg:top-1 lg:left-72 xl:top-8 xl:left-33 2xl:left-63 2xl:top-120">
           <ButtonOptions />
         </div>
-        <div className="absolute opacity-0 group-hover:opacity-100 transition duration-300 2xl:top-177 2xl:left-128 xl:top-55 xl:left-110 lg:top-68 lg:left-127 md:top-58 sm:top-280 sm:w-170 sm:h-65 bg-[#000061] 2xl:w-103 2xl:h-45 xl:w-90 xl:h-45 lg:w-113 lg:h-45 md:w-93 md:h-35 rounded-b-2xl text-sm border-l-2 border-r-2 border-b-2 border-[#1dd2af]">
+        <div className="absolute opacity-100 group-hover:opacity-100 transition duration-300 2xl:top-177 2xl:left-128 xl:top-55 xl:left-110 lg:top-70 lg:left-127 md:top-58 sm:top-280 sm:w-170 sm:h-65 bg-[#000061] 2xl:w-103 2xl:h-45 xl:w-90 xl:h-45 lg:w-113 lg:h-45 md:w-93 md:h-35 rounded-b-2xl text-sm border-l-2 border-r-2 border-b-2 border-[#1dd2af]">
           <div className="2xl:mt-5 2xl:ml-2 sm:ml-5 sm:mt-2 md:ml-0.5">
             <p className="2xl:ml-3 2xl:mt-2 font-bold 2xl:text-xl xl:text-xl md:text-lg xl:mt-4 xl:ml-5 lg:text-lg lg:ml-4 lg:mt-4 md:ml-4 md:mt-2 md:mb-1 sm:text-2xl sm:mb-10 sm:mt-3">
-              {movieData?.title?.[5]?.title} ({movieData?.title?.[1]?.released})
+              {movieData?.title?.[5]?.title} ({movieData?.title?.[4]?.released})
             </p>
-            <p className="2xl:ml-3 2xl:mt-1 2xl:mr-3 2xl:text-lg xl:ml-5 xl:mr-3 xl:mt-3 xl:text-md lg:text-md lg:ml-4 lg:mr-5 lg:mt-5 md:ml-4 md:mr-4 md:text-sm font-light sm:text-xl sm:mr-5 sm:mb-16">
+            <p className="2xl:ml-3 2xl:mt-1 2xl:mr-6 2xl:text-md xl:ml-5 xl:mr-3 xl:mt-3 xl:text-md lg:text-md lg:ml-4 lg:mr-5 lg:mt-5 md:ml-4 md:mr-4 md:text-sm font-light sm:text-xl sm:mr-5 sm:mb-16">
               {movieData?.title?.[5]?.synopsis}
             </p>
-            <p className="bg-[#1dd2af] rounded-4xl 2xl:mr-102 2xl:ml-2 2xl:-mt-11 2xl:p-2 2xl:w-14 xl:-mt-8 xl:p-2 xl:w-14 xl:ml-4 lg:w-13 lg:ml-4 lg:-mt-10 lg:p-2 md:w-13 md:p-2 md:ml-4 md:-mt-13 sm:p-2 sm:w-13 font-light text-xs">
+            <p className="bg-[#1dd2af] rounded-4xl 2xl:mr-102 2xl:ml-2 2xl:p-2 2xl:w-14 xl:-mt-13 2xl:-mt-7 xl:p-2 xl:w-14 xl:ml-4 lg:w-13 lg:ml-4 lg:-mt-10 lg:p-2 md:w-13 md:p-2 md:ml-4 md:-mt-13 sm:p-2 sm:w-13 font-light text-xs">
               {movieData?.title?.[5]?.genre}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="group 2xl:-mb-180 lg:mr-10 lg:mb-4 md:mr-10 sm:mb-5 shadow-teal-500 shadow-md rounded-2xl">
+        <img
+          src={movieData?.title?.[2]?.image}
+          alt="placeholder image"
+          className="rounded-2xl border-2 border-[#1dd2af] xl:-mt-89 2xl:-mt-151"
+        />
+        <div className="absolute opacity-100 group-hover:opacity-100 transition duration-300 sm:top-351 md:-left-75 md:top-10 lg:-left-55 lg:top-1 xl:-left-77 xl:top-8 2xl:top-120 2xl:-left-65">
+          <ButtonOptions />
+        </div>
+        <div className="absolute opacity-100 group-hover:opacity-100 transition duration-300 2xl:top-177 xl:top-55 lg:top-70 md:top-170 sm:top-455 sm:w-170 sm:h-65 bg-[#000061] 2xl:w-103 2xl:h-45 xl:w-90 xl:h-45 lg:w-113 lg:h-45 md:w-93 md:h-35 rounded-b-2xl text-sm border-l-2 border-r-2 border-b-2 border-[#1dd2af]">
+          <div className="2xl:mt-5 2xl:ml-2 sm:ml-5 sm:mt-2 md:ml-0.5">
+            <p className="2xl:ml-3 2xl:mt-2 font-bold 2xl:text-xl xl:text-xl md:text-lg xl:mt-4 xl:ml-5 lg:text-lg lg:ml-4 lg:mt-4 md:ml-4 md:mt-2 md:mb-1 sm:text-2xl sm:mb-10 sm:mt-3">
+              {movieData?.title?.[2]?.title} ({movieData?.title?.[2]?.released})
+            </p>
+            <p className="2xl:ml-3 2xl:mt-1 2xl:mr-3 2xl:text-lg xl:ml-5 xl:mr-3 xl:mt-3 xl:text-md lg:text-md lg:ml-4 lg:mr-5 lg:mt-5 md:ml-4 md:mr-4 md:text-sm font-light sm:text-xl sm:mr-5 sm:mb-16">
+              {movieData?.title?.[2]?.synopsis}
+            </p>
+            <p className="bg-[#1dd2af] rounded-4xl 2xl:mr-102 2xl:ml-2 2xl:-mt-11 2xl:p-2 2xl:w-14 xl:-mt-8 xl:p-2 xl:w-14 xl:ml-4 lg:w-13 lg:ml-4 lg:-mt-10 lg:p-2 md:w-13 md:p-2 md:ml-4 md:-mt-13 sm:p-2 sm:w-13 font-light text-xs">
+              {movieData?.title?.[2]?.genre}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="group 2xl:-mb-180 lg:mr-10 lg:mb-4 md:mr-10 sm:mb-5 shadow-teal-500 shadow-md rounded-2xl">
+        <img
+          src={movieData?.title?.[3]?.image}
+          alt="placeholder image"
+          className="rounded-2xl border-2 border-[#1dd2af] xl:-mt-89 2xl:-mt-151"
+        />
+        <div className="absolute opacity-100 group-hover:opacity-100 transition duration-300 sm:top-526 md:-left-75 md:top-10 lg:-left-55 lg:top-1 xl:-left-77 xl:top-8 2xl:top-120 2xl:-left-65">
+          <ButtonOptions />
+        </div>
+        <div className="absolute opacity-100 group-hover:opacity-100 transition duration-300 2xl:top-177 xl:top-55 lg:top-70 md:top-170 sm:top-630 sm:w-170 sm:h-65 bg-[#000061] 2xl:w-103 2xl:h-45 xl:w-90 xl:h-45 lg:w-113 lg:h-45 md:w-93 md:h-35 rounded-b-2xl text-sm border-l-2 border-r-2 border-b-2 border-[#1dd2af]">
+          <div className="2xl:mt-5 2xl:ml-2 sm:ml-5 sm:mt-2 md:ml-0.5">
+            <p className="2xl:ml-3 2xl:mt-2 font-bold 2xl:text-xl xl:text-xl md:text-lg xl:mt-4 xl:ml-5 lg:text-lg lg:ml-4 lg:mt-4 md:ml-4 md:mt-2 md:mb-1 sm:text-2xl sm:mb-10 sm:mt-3">
+              {movieData?.title?.[3]?.title} ({movieData?.title?.[3]?.released})
+            </p>
+            <p className="2xl:ml-3 2xl:mt-1 2xl:mr-3 2xl:text-lg xl:ml-5 xl:mr-3 xl:mt-3 xl:text-md lg:text-md lg:ml-4 lg:mr-5 lg:mt-5 md:ml-4 md:mr-4 md:text-sm font-light sm:text-xl sm:mr-5 sm:mb-16">
+              {movieData?.title?.[3]?.synopsis}
+            </p>
+            <p className="bg-[#1dd2af] rounded-4xl 2xl:mr-102 2xl:ml-2 2xl:-mt-11 2xl:p-2 2xl:w-14 xl:-mt-8 xl:p-2 xl:w-14 xl:ml-4 lg:w-13 lg:ml-4 lg:-mt-10 lg:p-2 md:w-13 md:p-2 md:ml-4 md:-mt-13 sm:p-2 sm:w-13 font-light text-xs">
+              {movieData?.title?.[3]?.genre}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="group 2xl:-mb-180 lg:mr-10 lg:mb-4 md:mr-10 sm:mb-5 shadow-teal-500 shadow-md rounded-2xl md:mt-10">
+        <img
+          src={movieData?.title?.[0]?.image}
+          alt="placeholder image"
+          className="rounded-2xl border-2 border-[#1dd2af] xl:-mt-89 2xl:-mt-151"
+        />
+        <div className="absolute opacity-100 group-hover:opacity-100 transition duration-300 sm:top-701 md:-left-75 md:top-10 lg:-left-55 lg:top-1 xl:-left-77 xl:top-8 2xl:top-120 2xl:-left-65">
+          <ButtonOptions />
+        </div>
+        <div className="absolute opacity-100 group-hover:opacity-100 transition duration-300 2xl:top-177 xl:top-55 lg:top-70 md:top-272 sm:top-805 sm:w-170 sm:h-65 bg-[#000061] 2xl:w-103 2xl:h-45 xl:w-90 xl:h-45 lg:w-113 lg:h-45 md:w-93 md:h-35 rounded-b-2xl text-sm border-l-2 border-r-2 border-b-2 border-[#1dd2af]">
+          <div className="2xl:mt-5 2xl:ml-2 sm:ml-5 sm:mt-2 md:ml-0.5">
+            <p className="2xl:ml-3 2xl:mt-2 font-bold 2xl:text-xl xl:text-xl md:text-lg xl:mt-4 xl:ml-5 lg:text-lg lg:ml-4 lg:mt-4 md:ml-4 md:mt-2 md:mb-1 sm:text-2xl sm:mb-10 sm:mt-3">
+              {movieData?.title?.[0]?.title} ({movieData?.title?.[0]?.released})
+            </p>
+            <p className="2xl:ml-3 2xl:mt-1 2xl:mr-3 2xl:text-lg xl:ml-5 xl:mr-3 xl:mt-3 xl:text-md lg:text-md lg:ml-4 lg:mr-5 lg:mt-5 md:ml-4 md:mr-4 md:text-sm font-light sm:text-xl sm:mr-5 sm:mb-16">
+              {movieData?.title?.[0]?.synopsis}
+            </p>
+            <p className="bg-[#1dd2af] rounded-4xl 2xl:mr-102 2xl:ml-2 2xl:-mt-11 2xl:p-2 2xl:w-14 xl:-mt-8 xl:p-2 xl:w-14 xl:ml-4 lg:w-13 lg:ml-4 lg:-mt-10 lg:p-2 md:w-13 md:p-2 md:ml-4 md:-mt-13 sm:p-2 sm:w-13 font-light text-xs">
+              {movieData?.title?.[0]?.genre}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="group 2xl:-mb-180 lg:mr-10 lg:mb-4 md:mr-10 sm:mb-5 shadow-teal-500 shadow-md rounded-2xl md:mt-10">
+        <img
+          src={movieData?.title?.[1]?.image}
+          alt="placeholder image"
+          className="rounded-2xl border-2 border-[#1dd2af] xl:-mt-89 2xl:-mt-151"
+        />
+        <div className="absolute opacity-0 group-hover:opacity-100 transition duration-300 sm:top-876 md:-left-75 md:top-10 lg:-left-55 lg:top-1 xl:-left-77 xl:top-8 2xl:top-120 2xl:-left-65">
+          <ButtonOptions />
+        </div>
+        <div className="absolute opacity-100 group-hover:opacity-100 transition duration-300 2xl:top-177 xl:top-55 lg:top-70 md:top-272 sm:top-980 sm:w-170 sm:h-65 bg-[#000061] 2xl:w-103 2xl:h-45 xl:w-90 xl:h-45 lg:w-113 lg:h-45 md:w-93 md:h-35 rounded-b-2xl text-sm border-l-2 border-r-2 border-b-2 border-[#1dd2af]">
+          <div className="2xl:mt-5 2xl:ml-2 sm:ml-5 sm:mt-2 md:ml-0.5">
+            <p className="2xl:ml-3 2xl:mt-2 font-bold 2xl:text-xl xl:text-xl md:text-lg xl:mt-4 xl:ml-5 lg:text-lg lg:ml-4 lg:mt-4 md:ml-4 md:mt-2 md:mb-1 sm:text-2xl sm:mb-10 sm:mt-3">
+              {movieData?.title?.[1]?.title} ({movieData?.title?.[1]?.released})
+            </p>
+            <p className="2xl:ml-3 2xl:mt-1 2xl:mr-3 2xl:text-lg xl:ml-5 xl:mr-3 xl:mt-3 xl:text-md lg:text-md lg:ml-4 lg:mr-5 lg:mt-5 md:ml-4 md:mr-4 md:text-sm font-light sm:text-xl sm:mr-5 sm:mb-16">
+              {movieData?.title?.[1]?.synopsis}
+            </p>
+            <p className="bg-[#1dd2af] rounded-4xl 2xl:mr-102 2xl:ml-2 2xl:-mt-11 2xl:p-2 2xl:w-14 xl:-mt-8 xl:p-2 xl:w-14 xl:ml-4 lg:w-13 lg:ml-4 lg:-mt-10 lg:p-2 md:w-13 md:p-2 md:ml-4 md:-mt-13 sm:p-2 sm:w-13 font-light text-xs">
+              {movieData?.title?.[1]?.genre}
             </p>
           </div>
         </div>
