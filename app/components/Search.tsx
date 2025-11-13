926a45c171d8a141cc032b714ede0ecd990158ad
zack6yuan <zack6yuan@gmail.com>
2025-11-13 13:54:59 -0600

    update search function.

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,12 +1,24 @@
+"use client"
+import { useState } from "react";
+
 export default function Search() {
+  const [search, setSearch] = useState('');
+
+  const handleSearchChange = (e) => {
+    const searchInput = e.target.value;
+    setSearch(searchInput)
+  }
+
   return (
     <div className="lg:mr-5">
       <div className="search-container">
         <p>Search</p>
         <input
           type="text"
+          value={search}
           placeholder="Search Movies..."
           className="border-2 rounded-3xl h-10 border-teal-400 bg-[#000061] pl-3 mt-2 mb-2 w-100"
+          onChange={handleSearchChange}
         />
       </div>
       <div className="year-input flex justify-between">
